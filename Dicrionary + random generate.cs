27abe91e6a228164094be54b5ce5583d4fcae8d3
Avey <45832714+2Avey@users.noreply.github.com>
2023-03-24 14:39:39 +0200

    Create Dicrionary + random generate.cs

---
+++ Dicrionary + random generate.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> arrayDict = new Dictionary<int, int>();
+
+            Console.Write("how mcuh dictionary elements create?: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random rand= new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                int random = rand.Next(0,1000);
+                arrayDict.Add(i, random);
+            }
+            foreach (var item in arrayDict)
+            {
+                Console.WriteLine($"{item.Key} - index | {item.Value} - value");
+            }
+        }
+        
+    }
+}

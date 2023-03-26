1db95928ab1f3279d3515ee6f428419c75894562
Avey <45832714+2Avey@users.noreply.github.com>
2023-03-26 20:35:07 +0300

    Update get, set.cs

--- get, set.cs
+++ get, set.cs
@@ -10,56 +10,16 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-
-
-
-            /*Console.WriteLine(GetLastHalf("I love CSharp!"));
-            Console.WriteLine(GetLastHalf("1234567890"));
-            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
-
-            static string GetLastHalf(string text)
-            {
-                int mid = text.Length / 2;
-                string str = text.Substring(mid);
-                str = str.Replace(" ", "");
-                return str;
-            }
-            G();
-            H();
-        }
-        static string who = "class";
-
-        static void F()
-        {
-            string who = "F";
-        }
-
-        static void G()
-        {
-            F();
-            Console.WriteLine(who);
-        }
-
-        static void H()
-        {
-            string who = "H";
-            F();
-            Console.Write(who);*/
-
             Player player = new Player();
             player.Name = "admin";
 
             player.SetName("admin");
         }
-        
-
     }
+    
     class Player
     {
-        
-        
         public string Test { get; set; }
-        
         public string Test1 ()
         {
             return null;
